03bdc49b0527f19aad691f3f28197ea7c6437d0b
njudd <nickkjudd@gmail.com>
2018-03-22 15:39:36 +0100

    Made a full merge, dots to commas for the swedish system also NAs to blank space and a package to write in .sav

--- Spss_merge.R
+++ Spss_merge.R
@@ -1,5 +1,4 @@
 #merging SPSS
-
 library(foreign)
 library(tidyverse)
 library(data.table)
@@ -13,10 +12,25 @@ x <- read_csv("Tau_wmgrid_alltrials.csv")
 names(x)[1] <- "uuid"
 names(x)[4] <- "tau"
 names(x)[8] <- "mean"
-
 x <- dcast(setDT(x), uuid ~ Problem.Level, value.var = c("tau", "mu", "ratio", "mean", "n_trials"), fill = NA) 
+x <- as_tibble(x)
 
+#solving the fucking comma issue, seriously a pain in the ass because of REGEX
+keep2 <- x[2:16]
+keep2 <- as.data.frame(lapply(keep2, function(y) gsub("\\.", ",", y)))
+x[2:16] <- keep2
+
+# joining the existing SPSS file to the new vars (i.e. x)
 fullspss <- fullspss %>% 
   left_join(x, by = c("uuid"))
-write_csv(fullspss, "fullspssfile.csv")
 
+#spss is a crap program that uses blank spaces as NA's
+fullspss<- sapply(fullspss, as.character)
+fullspss[is.na(fullspss)] <- "        "
+fullspss <- as_tibble(fullspss)
+
+#this library should be called heven if it actually works
+library(haven)
+write_sav(fullspss, "MASTER_UBER_willwork.sav")
+
+#write_csv(fullspss, "fullspssfile.csv")
